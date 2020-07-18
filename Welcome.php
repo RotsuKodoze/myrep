5bc3b43222b5ccb1130ed89459f1888efbf0d1d1
aguinaldoian <fujisakiian@gmail.com>
2020-07-18 22:28:57 +0800

    Initial commit

---
+++ Welcome.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+
+<style type="text/css">
+	body {
+		margin: 0;
+		padding: 0;
+		background-color: #34495e;
+		font-family: sans-serif;
+	}
+
+	#box {
+		width: 100%;
+		padding: 40px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background: #191919;
+		text-align: center;
+		}
+
+	#box h1 , #box p{
+		text-transform: uppercase;
+		color: #2ecc71;
+		font-weight: 600;
+	}
+
+	#box a {
+		color: #2980b9;
+		cursor: pointer;
+	}
+
+</style>
+
+</head>
+<body>
+	<form id="box">
+		<h1>welcome!</h1>	
+	</form>
+	
+</body>
+</html>
